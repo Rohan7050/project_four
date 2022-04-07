6266d93346a773e23565329ef04f3f93dc4f5dfc
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-04-07 16:06:54 +0530

    add caches in post req also

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -25,6 +25,7 @@ redisClient.on("connect", async function (){
 
 const SET_ASYNC = util.promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = util.promisify(redisClient.GET).bind(redisClient);
+const EXP_ASYNC = util.promisify(redisClient.expire).bind(redisClient)
 
 const isValid = (value) => {
     if (typeof value === 'undefined' || value === null) return false
@@ -50,9 +51,13 @@ const createUrl = async (req, res) => {
         data.longUrl = url
         const isUrlCodeInUse = await urlModel.findOne({longUrl: data.longUrl})
         if (isUrlCodeInUse){
+            // await SET_ASYNC(`${isUrlCodeInUse.urlCode}`, JSON.stringify(isUrlCodeInUse))
+            // await EXP_ASYNC(`${isUrlCodeInUse.urlCode}`, 60)
             return res.status(200).send({status: true, message: "already have short url for this long url" , data: isUrlCodeInUse})
         } 
         const shortend = await urlModel.create(data)
+        await SET_ASYNC(`${data.urlCode}`, JSON.stringify(shortend))
+        await EXP_ASYNC(`${data.urlCode}`, 60)
         return res.status(201).send({status: true, data: shortend})
     }catch(error){
         res.status(500).send({status: false, message: error.message})
@@ -68,8 +73,8 @@ const redirectTo = async (req, res) => {
         let check = await GET_ASYNC(`${urlCode}`)
         if (check){
             let doc = JSON.parse(check)
-            // return res.status(200).send({from: "caches", data: docl})
             console.log("from caches")
+            // return res.status(200).send({from: "caches", data: doc})
             return res.status(302).redirect(doc.longUrl)
         }else{
             const isUrlCodePresent = await urlModel.findOne({urlCode: urlCode})
@@ -77,8 +82,10 @@ const redirectTo = async (req, res) => {
                 return res.status(404).send({status: false, message: "this url does not exist"})
             }
             await SET_ASYNC(`${urlCode}`, JSON.stringify(isUrlCodePresent))
+            await EXP_ASYNC(`${urlCode}`, 60)
+            // await redisClient.expire(`${urlCode}`, 30)
             console.log("from MongoDB")
-            res.status(302).redirect(isUrlCodePresent.longUrl)
+            return res.status(302).redirect(isUrlCodePresent.longUrl)
             // return res.status(200).send({from: "MongoDb", data: isUrlCodePresent})
         }
     }catch(error){
