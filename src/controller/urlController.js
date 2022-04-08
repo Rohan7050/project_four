1923f50aaea413b93763ec771c7de9e675ad1c80
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-04-08 09:37:11 +0530

    add 3 min expire time

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -52,12 +52,12 @@ const createUrl = async (req, res) => {
         const isUrlCodeInUse = await urlModel.findOne({longUrl: data.longUrl})
         if (isUrlCodeInUse){
             // await SET_ASYNC(`${isUrlCodeInUse.urlCode}`, JSON.stringify(isUrlCodeInUse))
-            // await EXP_ASYNC(`${isUrlCodeInUse.urlCode}`, 60)
+            // await EXP_ASYNC(`${isUrlCodeInUse.urlCode}`, 60*3)
             return res.status(200).send({status: true, message: "already have short url for this long url" , data: isUrlCodeInUse})
         } 
         const shortend = await urlModel.create(data)
         await SET_ASYNC(`${data.urlCode}`, JSON.stringify(shortend))
-        await EXP_ASYNC(`${data.urlCode}`, 60)
+        await EXP_ASYNC(`${data.urlCode}`, 60*3)
         return res.status(201).send({status: true, data: shortend})
     }catch(error){
         res.status(500).send({status: false, message: error.message})
@@ -82,8 +82,8 @@ const redirectTo = async (req, res) => {
                 return res.status(404).send({status: false, message: "this url does not exist"})
             }
             await SET_ASYNC(`${urlCode}`, JSON.stringify(isUrlCodePresent))
-            await EXP_ASYNC(`${urlCode}`, 60)
-            // await redisClient.expire(`${urlCode}`, 30)
+            // add expire time 3 min
+            await EXP_ASYNC(`${urlCode}`, 60*3)
             console.log("from MongoDB")
             return res.status(302).redirect(isUrlCodePresent.longUrl)
             // return res.status(200).send({from: "MongoDb", data: isUrlCodePresent})
